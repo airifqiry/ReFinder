d158ab16f2995fe13e81842d556ac30b31657136
Airis Yasenova <airisyasenova@Airiss-MacBook-Pro.local>
2025-04-10 20:36:17 +0300

    folders and registration

---
+++ ReFind/ReFinds/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    first_name= forms.CharField(label='Име',max_length=30,required=True)
+    last_name= forms.CharField(label='Фамилия',max_length=50,required=True)
+    email = forms.EmailField(required=True, label="Имейл адрес")
+
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Потребителско име',
+            'password1': 'Парола',
+            'password2': 'Потвърди паролата',
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Този имейл вече е регистриран.")
+        return email
--- ReFind/ReFinds/views.py
+++ ReFind/ReFinds/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.contrib import messages
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрацията беше успешна! Моля, влез в профила си.')
+            return redirect('login')  
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'register.html', {'form': form})
+
