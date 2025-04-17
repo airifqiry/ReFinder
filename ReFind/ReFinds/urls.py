79ece107372e1738961f74b60d52317f92553f89
deyaNaydenova <deya.naydenova2010@gmail.com>
2025-04-17 13:42:25 +0300

    logout

--- ReFind/ReFinds/urls.py
+++ ReFind/ReFinds/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('',views.index_view,name='index'),
+    path('', views.index_view, name='index'),
     path('home/', views.home_view, name='home'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/',views.user_logout,name='logout'),
+    path('logout/', views.user_logout, name='logout'),
 ]
-
