0e3029b6732a5b6cfc9889bd5c20a42b91e28b3c
Airis Yasenova <airisyasenova@Airiss-MacBook-Pro.local>
2025-04-19 15:06:59 +0300

    changes

--- ReFind/ReFinds/models.py
+++ ReFind/ReFinds/models.py
@@ -24,5 +24,20 @@ class Ad(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 
 
